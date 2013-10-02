0094acd60e89fbdee372b6e51552d7de72749096
omar <omar@piwik.xflogger.com>
2013-10-02 09:41:34 -0400

    Move error_reporting call to the top in order to allow bootstrap.php to change it if it needs to

--- index.php
+++ index.php
@@ -12,12 +12,13 @@ use Piwik\FrontController;
 use Piwik\Error;
 use Piwik\ExceptionHandler;
 
+error_reporting(E_ALL | E_NOTICE);
+
 define('PIWIK_DOCUMENT_ROOT', dirname(__FILE__) == '/' ? '' : dirname(__FILE__));
 if (file_exists(PIWIK_DOCUMENT_ROOT . '/bootstrap.php')) {
     require_once PIWIK_DOCUMENT_ROOT . '/bootstrap.php';
 }
 
-error_reporting(E_ALL | E_NOTICE);
 @ini_set('display_errors', defined('PIWIK_DISPLAY_ERRORS') ? PIWIK_DISPLAY_ERRORS : @ini_get('display_errors'));
 @ini_set('xdebug.show_exception_trace', 0);
 @ini_set('magic_quotes_runtime', 0);
